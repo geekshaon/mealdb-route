3764c219265ac10cac5787aed73e5c3624085bae
geekshaon <geekshaon@gmail.com>
2024-05-23 10:20:52 +0600

    active link and loading added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,31 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import ActiveLink from "../extra/ActiveLink";
+import "./Header.css";
 
 const Header = () => {
+  const Navigation = useNavigation();
   return (
     <div>
       <nav className="container mx-auto bg-slate-900 px-3 py-3 rounded-lg flex justify-around text-slate-50 items-center">
         <h2 className="text-2xl">MealDb by RexShaon</h2>
-        <div className="meanu">
-          <Link className="px-2" to="/">
+        <div className="menu flex">
+          <ActiveLink className="px-2" to="/">
             Home
-          </Link>
-          <Link className="px-2" to="/meals">
+          </ActiveLink>
+          <ActiveLink className="px-2" to="/meals">
+            Meals
+          </ActiveLink>
+          <ActiveLink className="px-2" to="/about">
             About
-          </Link>
-          <Link className="px-2" to="/">
-            Meal
-          </Link>
+          </ActiveLink>
         </div>
       </nav>
-      <div className="container mx-auto">
+      <div className="container mx-auto text-center">
+        {Navigation.state == "loading" ? "Loading boss" : ""}
+      </div>
+
+      <div className="container mx-auto ">
         <Outlet></Outlet>
       </div>
     </div>
